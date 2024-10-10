814cddbaf3cf5192bece3090bb59837ad4bfd6dc
Rafael Araújo <137319648+leafaraujo@users.noreply.github.com>
2024-10-10 13:45:26 -0300

    Update 04.java

--- 1. List/04.java
+++ 1. List/04.java
@@ -1,15 +1,20 @@
 import java.util.Scanner;
 import java.text.DecimalFormat;
 
-  
+
 public class Main {
-  
+
   public static void main(String[] args){
-    final double SIDE = 2.6;
-    Scanner sc = new Scanner(System.in);
+    Scanner input = new Scanner(System.in);
     DecimalFormat deci = new DecimalFormat("0.00");
-    double perimeter = SIDE * 3;
-    System.out.println("LADO: " + SIDE);
+    System.out.println("Digite quais são os valores dos lados A, B e C de seu triângulo: ");
+    float A = input.nextFloat();
+    float B = input.nextFloat();
+    float C = input.nextFloat();
+    double perimeter = A + B + C;
+    System.out.println("LADO A: " + A);
+    System.out.println("LADO B: " + B);
+    System.out.println("LADO C: " + C);
     System.out.println("PERÍMETRO: " + deci.format(perimeter));
     }
   }
