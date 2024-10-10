e55c4a95753663e8de455e4c25fc4ce0e672e5a9
Rafael Araújo <137319648+leafaraujo@users.noreply.github.com>
2024-10-10 13:35:48 -0300

    Update 01.java

--- 1. List/01.java
+++ 1. List/01.java
@@ -1,13 +1,16 @@
 import java.text.DecimalFormat;
- 
+import java.util.Scanner;
+
 public class Main {
-  
+
   public static void main(String[] args){
-    final double BASE = 7.5;
-    final double HEIGHT = 12.3;
+    Scanner input = new Scanner(System.in);
+    System.out.println("Digite a base e altura do seu retângulo, respectivamente: ");
+    double base = input.nextDouble();
+    double height = input.nextDouble();
     DecimalFormat deci = new DecimalFormat("0.00");
-    double perimeter = 2 * (BASE + HEIGHT);
-    double size = BASE * HEIGHT;
+    double perimeter = 2 * (base + height);
+    double size = base * height;
     System.out.println("ÁREA: " + deci.format(size));
     System.out.println("PERÍMETRO: " + deci.format(perimeter));
     }
