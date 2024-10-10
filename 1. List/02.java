4c57ce3989a7cf27208f2abbed1467c95fb29387
Rafael Araújo <137319648+leafaraujo@users.noreply.github.com>
2024-10-10 13:38:17 -0300

    Update 02.java

--- 1. List/02.java
+++ 1. List/02.java
@@ -1,14 +1,16 @@
 import java.text.DecimalFormat;
+import java.util.Scanner;
 
-  
 public class Main {
-  
+
   public static void main(String[] args){
-    final double SIDE = 8;
+    Scanner input = new Scanner(System.in);
+    System.out.println("Digite qual o lado do quadrado: ");
+    double side = input.nextDouble();
     DecimalFormat deci = new DecimalFormat("0.00");
-    double perimeter = SIDE * 4;
-    double size = SIDE * SIDE;
+    double perimeter = side * 4;
+    double size = side * side;
     System.out.println("ÁREA: " + deci.format(size));
     System.out.println("PERÍMETRO: " + deci.format(perimeter));
-    }
   }
+}
