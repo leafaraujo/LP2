98fbab716ff05d6919c881c013a8a323aaad5cfa
Rafael Araújo <137319648+leafaraujo@users.noreply.github.com>
2024-10-10 13:41:39 -0300

    Update 03.java

--- 1. List/03.java
+++ 1. List/03.java
@@ -1,15 +1,18 @@
 import java.text.DecimalFormat;
+import java.util.Scanner;
 
-  
 public class Main {
-  
+
   public static void main(String[] args){
-    final double RADIUS = 2.8;
+    Scanner input = new Scanner(System.in);
+    System.out.println("Digite qual o raio de sua circunferência: ");
+    double radius = input.nextDouble();
     final double PI = 3.14;
     DecimalFormat deci = new DecimalFormat("0.00");
-    double perimeter = 2 * PI * RADIUS;
-    double size = PI * (RADIUS * RADIUS);
+    double perimeter = 2 * PI * radius;
+    double size = PI * (radius * radius);
     System.out.println("ÁREA: " + deci.format(size));
     System.out.println("PERÍMETRO: " + deci.format(perimeter));
     }
-  }
+  
+}
