4b237878767aa672b1015435572cd9e180e4e880
Rafael Araújo <137319648+leafaraujo@users.noreply.github.com>
2024-10-16 19:33:17 -0300

    Create Book.java

---
+++ 4.list/Library/Book.java
@@ -0,0 +1,43 @@
+package Library;
+
+public class Book{
+  private String bookTitle;
+  private String bookAutor;
+  private String bookID;
+  private String bookGender;
+  private String userID;
+  private int bookEdition;
+  private int quantityOfPages;
+  private boolean isAvailable = true;
+
+  public Book(String title, String autor, int edition, int pages, Stringt ID){
+    this.bookTitle = title;
+    this.bookAutor = autor;
+    this.bookEdition = edition;
+    this.bookID = ID;
+  }
+
+  public String getTitle(){
+    return this.bookTitle;
+  }
+  
+  public String getID(){
+    return this.bookID;
+  }
+
+  borrowABook(String userID){
+    if(this.isAvailable){
+      isAvailable = false;
+      this.userID = userID;
+    }
+    else{
+      System.out.println("Esse livro está emprestado para o usuário " + this.userID);
+    }
+  }
+
+  returnABook(){
+    if(!this.isAvailable){
+      isAvailable = true;
+    }
+  }
+}
