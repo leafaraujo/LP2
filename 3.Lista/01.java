31104cdd5c801e1d3e38af7df38db6874190b1de
Rafael Araújo <137319648+leafaraujo@users.noreply.github.com>
2024-10-17 15:39:36 -0300

    Create 01.java

---
+++ 3.Lista/01.java
@@ -0,0 +1,32 @@
+import java.util.scanner
+
+ public class Main{
+
+    public static void printVector(int[] vector, int lenght){
+      System.out.print("[");
+      for(int i = lenght; i >= 0; i--){
+        if(i == 0){
+          System.out.println(vector[i] + "]");
+        }
+        else{
+          System.out.printf("%d, ", vector[i]);
+        }
+      }
+    }
+  
+  public static void main(String[] args){
+    
+    Scanner input = new Scanner(System.in);
+    int vectorSize = 0, num = 0;
+    System.out.print("Insira o tamanho do vetor: ");
+    vectorSize = input.nextInt();
+    int vector[] = new int[vectorSize];
+    System.out.print("Agora digite os elementos (a quantidade deve ser igual a " + vectorSize + "): ");
+    for(int i = 0; i < vectorSize; i++){
+      num = input.nextInt();
+      vector[i] = num;
+    }
+    System.out.print("O vetor é: ");
+    printVector(vector, vectorSize - 1);
+  }
+ }
