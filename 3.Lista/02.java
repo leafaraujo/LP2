28984415701d2ca930b9e0f1311f7e2c739a2293
Rafael Araújo <137319648+leafaraujo@users.noreply.github.com>
2024-10-17 16:14:55 -0300

    Create 02.java

---
+++ 3.Lista/02.java
@@ -0,0 +1,51 @@
+
+import java.util.Random;
+
+
+
+  public class Main{
+
+    public static int[] sortVector(int[] vector){
+      int j;
+      int key;
+      int i;
+
+      for (j = 1; j < vector.length; j++){
+        key = vector[j];
+        for (i = j - 1; (i >= 0) && (vector[i] > key); i--){
+          vector[i + 1] = vector[i];
+          }
+        vector[i + 1] = key;
+        }
+      return vector;
+    }
+
+    public static void printVector(int[] vector, int lenght){
+      System.out.print("[");
+      for(int i = lenght; i >= 0; i--){
+        if(i == 0){
+          System.out.println(vector[i] + "]");
+        }
+        else{
+          System.out.printf("%d, ", vector[i]);
+        }
+      }
+    }
+  
+  public static void main(String[] args){
+    
+    Scanner input = new Scanner(System.in);
+    int num = 0;
+
+    int vector[] = new int[10];
+    System.out.print("Agora digite os elementos (a quantidade deve ser igual a 10): ");
+    for(int i = 0; i < 10; i++){
+      num = input.nextInt();
+      vector[i] = num;
+    }
+    System.out.print("O vetor é: ");
+    vector = sortVector(vector);
+    printVector(vector, 10 - 1);
+  }
+}
+      
